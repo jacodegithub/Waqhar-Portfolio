8f84d2017aac221633ca497aee3867c48d216102
jacodegithub <dudeobama246@gmail.com>
2023-12-22 20:53:07 +0530

    updating portfolio

--- src/compoenents/experience/Experience.jsx
+++ src/compoenents/experience/Experience.jsx
@@ -11,33 +11,31 @@ export const Experience = () => {
                 <span>Experience</span>
             </div>
             <div className="experience-section-body">
+                <div className="experience-section-2">
                 <div className="experience-section-1">
                     <div className="experience-section-img-wrapper">
                         <img src={at_jindal} alt="at_jindal" className="experience-section-img" />
                     </div>
                 </div>
-                <div className="experience-section-2">
                     <div className="previous-experience-detail">
                         <div className='previous-experience'>Previous Experience:</div>
                         <div className='previous-experience-post'>Assistant Manager | Jindal Steel Works (JSW) | [Dec 2019 - Dec 2022]</div>
                         
                         <div className='previous-experience-desc'>
-                        <div>At Jindal Steel Works (JSW), India's largest steel manufacturing company, 
-                        I held the position of Assistant Manager as a production and 
-                        maintenance engineer. Over the course of my 3-year tenure.</div>
+                        <div>During my 3-year tenure at Jindal Steel Works (JSW), I served as an Assistant 
+                            Backend Developer, contributing to ERP model 
+                            project. In this role, I played a key part in enhancing 
+                            backend systems and ensuring their optimal performance.
+                        </div>
                         <br></br>
-                        <div>During my three-year tenure at JSW as an Assistant Manager, 
-                        I focused on maintaining smooth production operations and machinery. 
-                        Leading a team of ten, I minimized operational downtime and efficiently 
-                        addressed machinery issues. This experience enhanced my technical skills 
-                        and leadership abilities.</div>
+                        <div>During my initial year at JSW, I played a crucial role as an HR Assistant in managing the company's response to the COVID crisis. My responsibilities included overseeing employee well-being, addressing concerns related to the pandemic, and managing the onboarding and offboarding processes. Subsequently, I transitioned into the role of Assistant Manager, where I focused on maintaining seamless production operations and machinery efficiency. Leading a team of ten, I successfully minimized operational downtime and efficiently addressed machinery issues, further enhancing my technical skills and leadership abilities during my overall three-year tenure.</div>
                         <br></br>
-                        <div>Simultaneously, I managed contractor teams and played an active 
-                        role in site management, including inspections, permits, and plant 
-                        inductions. This multifaceted role sharpened my skills in teamwork, 
-                        communication, and project management. My time at JSW provided me 
-                        with a solid foundation in leadership and 
-                        project management that I'm eager to apply in my future endeavors.</div>
+                        <div>Contributed to the development of an ERP model 
+                            project, ensuring seamless integration and efficient 
+                            data management within the system. 
+                            Spearheaded the design and implementation of a 
+                            robust stock management application, enhancing
+                            inventory control and optimizing operations.</div>
                         </div>
                     </div>
                 </div>
@@ -45,7 +43,7 @@ export const Experience = () => {
             <div className="experience-bottom">
                 <a href="#projects">
                     <h2>
-                        Don't judge me by my background, check out my projects..
+                        Get a look @ my projects..
                     </h2>                       
                 </a>
             </div>
